91cfe9a23a7651b568db8d13b84df333b8a533b3
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-11-02 12:09:08 +0100

    profil deleted

--- routes/user.js
+++ routes/user.js
@@ -10,27 +10,37 @@ app.get('/me', passport.authenticate('jwt'), (req, res) => {
 
 app.put('/:id', passport.authenticate('jwt'), async (req, res) => {
     const { id } = req.params
-    
+
     try {
         const user = await User.update(req.body, {
             where: {
                 id,
             },
-        });
+        })
         const response = await User.findOne({
             where: {
                 id,
             },
-        });
+        })
 
-        res.json(response);
+        res.json(response)
     } catch (e) {
-        console.log(e);
-        res.status(500).json("Internal server error");
+        console.log(e)
+        res.status(500).json('Internal server error')
     }
-
 })
 
+app.delete('/:id', passport.authenticate('jwt'), async (req, res) => {
+    const { id } = req.params
+    try {
+        await User.destroy({
+            where: { id },
+        })
+        res.status(200).json('User deleted')
+    } catch (e) {
+        console.log(e)
+        res.status(500).json('Internal server error')
+    }
+})
 
-
-module.exports = app 
+module.exports = app
