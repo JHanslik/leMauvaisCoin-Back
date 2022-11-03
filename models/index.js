c9469b72ba70146bf25634d25259ad5c819807a6
JHanslik <julien.hanslik@gmail.com>
2022-11-03 10:41:54 +0100

    db jaws added

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('LeMauvaisCoin', 'root', '', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    logging: false,
-})
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+    }
+)
 
 const connectDb = async () => {
     try {
